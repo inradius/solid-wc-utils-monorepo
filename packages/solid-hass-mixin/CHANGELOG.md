e577a93d0e10cb101942765111469fef6900adce
Travis Stroud <travis@travisstroud.com>
2026-03-13 19:17:08 -0500

    release v0.0.1 @inradius/solid-wc-hass-mixin

---
+++ packages/solid-hass-mixin/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @inradius/solid-wc-hass-mixin
+
+## 0.0.1
+
+### Patch Changes
+
+- Create the initial withHomeAssistant component-register mixin
--- packages/solid-hass-mixin/src/index.ts
+++ packages/solid-hass-mixin/src/index.ts
@@ -1,1 +1,53 @@
-export {};
+import type { ComponentType } from 'component-register';
+
+export interface LovelaceCardConfig {
+  type: string;
+  [key: string]: unknown;
+}
+
+export interface CustomCardEntry {
+  type: string;
+  name: string;
+  description: string;
+  preview?: boolean;
+}
+
+export interface HomeAssistantOptions {
+  defaultConfig?: Record<string, unknown>;
+  customCard: CustomCardEntry;
+  cardSize?: number;
+}
+
+interface HomeAssistantElement extends HTMLElement {
+  config?: LovelaceCardConfig;
+  setConfig(config: LovelaceCardConfig): void;
+  getCardSize(): number;
+}
+
+const withHomeAssistant = (options: HomeAssistantOptions) => {
+  const { defaultConfig = {}, customCard, cardSize = 3 } = options;
+
+  return <T>(ElementType: ComponentType<T>): ComponentType<T> => {
+    const proto = (ElementType as CustomElementConstructor)
+      .prototype as HomeAssistantElement;
+    const win = window as unknown as Window & { customCards: unknown[] };
+    win.customCards = win.customCards || [];
+
+    proto.setConfig = function (config: LovelaceCardConfig) {
+      if (!config) {
+        throw new Error('Invalid configuration');
+      }
+      this.config = { ...defaultConfig, ...config };
+    };
+
+    proto.getCardSize = function () {
+      return cardSize;
+    };
+
+    win.customCards.push({ ...customCard });
+
+    return ElementType;
+  };
+};
+
+export default withHomeAssistant;
