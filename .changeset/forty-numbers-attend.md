fd9919c0d23f494065805705a1f4fd74aad9422f
Travis Stroud <travis@travisstroud.com>
2026-03-14 20:34:02 -0500

    feat: add an example tailwind card

---
+++ .changeset/forty-numbers-attend.md
@@ -0,0 +1,5 @@
+---
+'@inradius/solid-wc-hass-card': patch
+---
+
+Add tailwind card example
--- packages/solid-wc-hass-card/src/index.tsx
+++ packages/solid-wc-hass-card/src/index.tsx
@@ -18,9 +18,19 @@ const AppCore: Component = () => {
   });
 
   return (
-    <div class="text-[#555555]">
-      <h1>{heading()}</h1>
-      <p>{description()}</p>
+    <div class="w-full md:w-1/3 px-2">
+      <div class="rounded-lg shadow-sm mb-4">
+        <div class="rounded-lg bg-white shadow-lg md:shadow-xl relative overflow-hidden">
+          <div class="px-3 pt-8 pb-10 text-center relative z-10">
+            <h4 class="text-sm uppercase text-gray-500 leading-tight">
+              {heading()}
+            </h4>
+            <h3 class="text-2xl text-gray-700 font-semibold leading-tight my-3">
+              {description()}
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
